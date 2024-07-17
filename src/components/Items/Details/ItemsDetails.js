a8c7cc1ac39a5a24c70e365524b5c27d41121b15
SaadSleem80 <saadsleem80@gmail.com>
2024-07-17 14:31:57 +0300

    fixes for build

--- src/components/Items/Details/ItemsDetails.js
+++ src/components/Items/Details/ItemsDetails.js
@@ -92,15 +92,15 @@ const ItemsDetails = ({ type }) => {
             </Box>
             <Box sx={{ display: 'flex', mb: 3 }}>
               <Typography sx={{ mr: 2, fontWeight: 500, color: 'text.secondary' }}>{t('soruce_name')}:</Typography>
-              <Typography sx={{ color: 'text.secondary' }}>{type.source?.soruce_name}</Typography>
+              <Typography sx={{ color: 'text.secondary' }}>{type.source?.source_name}</Typography>
             </Box>
             <Box sx={{ display: 'flex', mb: 3 }}>
               <Typography sx={{ mr: 2, fontWeight: 500, color: 'text.secondary' }}>{t('soruce_address')}:</Typography>
-              <Typography sx={{ color: 'text.secondary' }}>{type.source?.soruce_address}</Typography>
+              <Typography sx={{ color: 'text.secondary' }}>{type.source?.source_address}</Typography>
             </Box>
             <Box sx={{ display: 'flex', mb: 3 }}>
               <Typography sx={{ mr: 2, fontWeight: 500, color: 'text.secondary' }}>{t('soruce_address')}:</Typography>
-              <Typography sx={{ color: 'text.secondary' }}>{type.source?.soruce_website}</Typography>
+              <Typography sx={{ color: 'text.secondary' }}>{type.source?.source_website}</Typography>
             </Box>
             <Box sx={{ display: 'flex', mb: 3 }}>
               <Typography sx={{ mr: 2, fontWeight: 500, color: 'text.secondary' }}>{t('sort')}:</Typography>
